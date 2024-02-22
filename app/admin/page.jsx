3a73388f0310f9fa6bacad065956734be4b62f3a
mtalhastar <mtalhastar@gmail.com>
2024-02-22 16:44:48 +0500

    done

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Admin Page</div>
+    <div className="flex justify-center mt-8">
+    <input
+      type="text"
+      placeholder="Search..."
+      className="border border-gray-300 rounded-full px-8 py-2 focus:outline-none focus:border-gray-600"
+    />
+  </div>
   )
 }
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,14 +2,16 @@
 import Link from "next/link"
 import {auth} from "../services/firebase"
 import {useEffect,useState } from 'react'
-
+import { Menu } from 'lucide-react';
 const Navbar = () => {
 
   const [currentUser, setCurrentUser] = useState(false);
-
+  const [menuOpen, setMenuOpen] = useState(false); 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
-      // This function will be called whenever the authentication state changes
       if (user){
         setCurrentUser(true);
       }else{
@@ -17,30 +19,39 @@ const Navbar = () => {
       }
      
     });
-
-    // Cleanup the observer when the component unmounts
     return () => unsubscribe();
   }, []); 
   
   return (
     <>
-   <div className="flex p-5 justify-between sticky">
-     <div>
-      <h1 className="font-bold text-xl" >IccyCream</h1>
+    {/* Menu button for mobile */}
+    <button
+      className="block p-3 md:hidden"
+      onClick={toggleMenu}
+    >
+     <Menu />
+    </button>
+
+    {/* Menu */}
+    <div className={`flex p-5 justify-between ${menuOpen ? 'block' : 'hidden'} md:flex`}>
+      <div>
+        <h1 className="font-bold text-xl">IccyCream</h1>
       </div>
-      <div className="flex space-x-10">
-      <Link href='/homepage'><h1  className="hover:text-red-500 cursor-pointer">Home</h1></Link> 
-      <Link href='/admin'> <h1 className="hover:text-red-500 cursor-pointer">Manage Products</h1></Link>
-      <Link href='/orders'> <h1 className="hover:text-red-500 cursor-pointer">Orders</h1></Link>
-      <Link href='/about'>  <h1 className="hover:text-red-500 cursor-pointer" >About</h1></Link>
-      {currentUser &&
-        <Link href='/signup'>  <h1 className="hover:text-red-500 cursor-pointer">Manage Users</h1></Link>
-      }
-      {currentUser &&
-         <h1 className="hover:text-red-500 cursor-pointer" onClick={()=>auth.signOut()}>SignOut</h1>
-      }
+      <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-10">
+        <Link href='/homepage'><h1 className="hover:text-red-500 cursor-pointer">Home</h1></Link>
+        {currentUser &&
+           <Link href='/admin'><h1 className="hover:text-red-500 cursor-pointer">Manage Products</h1></Link>
+        }
+        <Link href='/orders'><h1 className="hover:text-red-500 cursor-pointer">Orders</h1></Link>
+        <Link href='/about'><h1 className="hover:text-red-500 cursor-pointer">About</h1></Link>
+        {currentUser &&
+          <Link href='/signup'><h1 className="hover:text-red-500 cursor-pointer">Manage Users</h1></Link>
+        }
+        {currentUser &&
+          <h1 className="hover:text-red-500 cursor-pointer" onClick={() => auth.signOut()}>SignOut</h1>
+        }
       </div>
-   </div>
+    </div>
   </>
   )
 }
