3f6592627a3d4420f4b546a6c4e10c9090de8115
mtalhastar <mtalhastar@gmail.com>
2024-03-02 23:36:34 +0500

    done

---
+++ app/orders/component/ordercard.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import Image from 'next/image'
+const OrderCard = (props) => {
+  console.log(props)
+  const handleDelete = () => {
+    // Call the delete function passed through props
+    console.log(props.props.id)
+    props.deletefunction(props.props.id);
+  };
+  return (
+  <>
+   <div className="w-64 rounded overflow-hidden shadow-lg">
+  
+  <div className="px-6 py-4">
+  <div className="font-bold text-xl mb-2">OrderId</div>
+  <div className="font-light text-sm mb-2"> {props.props.id}</div>
+    <div className="font-bold text-xl mb-2">{props.props.city}</div>
+    <p className="text-gray-700 text-base">
+     {props.props.phone}
+    </p>
+    <p className="text-gray-700 text-base">
+     {props.props.items}
+    </p>
+    <div className="font-bold text-xl  mt-2">  {props.props.homeaddress}</div>
+    <div className="font-bold text-xl  mt-2">  {props.props.status}</div>
+  </div>
+  <div className="px-6  pb-2">
+    <span onClick={handleDelete} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 hover:bg-gray-300 hover:text-gray-800 cursor-pointer">Delivered</span>
+  </div>
+</div>
+  </>
+  )
+}
+
+export default OrderCard
--- app/orders/page.jsx
+++ app/orders/page.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react';
+import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore';
+import Card from './component/ordercard';
+import { db } from '../../services/firebase';
+import { useRouter } from 'next/navigation';
+import OrderCard from './component/ordercard';
 
-const page = () => {
-  return (
-    <div>Order Page</div>
-  )
+async function fetchDataFromFirestore() {
+  const querySnapshot = await getDocs(collection(db, "orders"));
+  const data = [];
+
+  querySnapshot.forEach((doc) => {
+    data.push({ id: doc.id, ...doc.data() });
+  });
+  return data;
 }
-export default page
+
+const Page = () => {
+  const [icecreams, setIcecreams] = useState();
+  const [filteredIcecreams, setFilteredIcecreams] = useState(); // State to hold filtered ice creams
+  const router = useRouter();
+  const [change, setChange] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchDataFromFirestore();
+      setIcecreams(data);
+      setFilteredIcecreams(data); // Initialize filtered ice creams with all ice creams
+    }
+    fetchData();
+  }, [change]);
+
+  const deleteDataFromFirestore = async (docid) => {
+    try {
+      await deleteDoc(doc(db, "orders", docid));
+      setChange(!change); // Toggle change to trigger re-fetch of data
+      console.log('Document successfully deleted');
+      return { success: true, message: "Document successfully deleted" };
+    } catch (error) {
+      console.error("Error deleting document: ", error);
+      return { success: false, message: "Error deleting document" };
+    }
+  }
+
+  const handleSearch = (event) => {
+    const searchTerm = event.target.value.toLowerCase();
+    const filteredData = icecreams.filter((icecream) =>
+      icecream.name.toLowerCase().includes(searchTerm)
+    );
+    setFilteredIcecreams(filteredData);
+  };
+
+  return (
+    <>
+      <div className="flex justify-center mt-8">
+        <input
+          type="text"
+          placeholder="Search"
+          className="border border-gray-300 rounded-full px-10 py-2 w-72 focus:outline-none focus:border-gray-600"
+          onChange={handleSearch}
+        />
+      </div>
+      <div className="flex justify-center">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 mt-8">
+          {filteredIcecreams && filteredIcecreams.map((icecream) =>
+            <OrderCard key={icecream.id} props={icecream} deletefunction={deleteDataFromFirestore} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Page;
